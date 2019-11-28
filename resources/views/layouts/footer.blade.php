ecc194cf9f084dad85d6331c44858ac3adaeca4e
Romain Valla <romainvalla@MacBook-Pro-de-Romain.local>
2019-11-28 11:17:07 +0100

    JS BUGFIX

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,6 +1,6 @@
 <footer>
 </footer>
-<script src="../js/script.js" charset="utf-8"></script>
+<script src="{{ URL::asset('/js/script.js') }}" charset="utf-8"></script>
 <script src="https://kit.fontawesome.com/d9a2e4a111.js" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/vue/dist/vue.js"></script>
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
