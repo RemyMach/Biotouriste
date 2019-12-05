f8bbaa60b711b110352ceb4d056824f0030e8455
Romain Valla <romainvalla@MacBook-Pro-de-Romain.local>
2019-12-05 20:34:49 +0100

    New front /!\

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -1,11 +1,11 @@
 <div class="navbar">
   <nav>
-    <a href="{{ url('announces') }}" class="underline">Annonces</a>
-    <a href="#" class="underline">Message</a>
-    <a href="#" class="underline">Map</a>
-    <a href="#" class="title">BioTourist</a>
-    <a href="#content-3" class="underline">Contact</a>
-    <a href="#" class="underline">Aide</a>
+    <a href="{{ url('announces') }}">About us</a>
+    <a href="#">Announces</a>
+    <a href="#">Products</a>
+    <a href="#" class="title">HEALTHY'S</a>
+    <a href="#content-3">Blog</a>
+    <a href="#">Contact us</a>
     <button id="btn-profil" class="btn-log sp-10" type="button" name="connexion" onclick="openSignup()"><i class="fas fa-user sp-30"></i></button>
     <button id="btn-cart" class="btn-cart sp-30" type="button" name="btn-cart" onclick="openCart()"><i class="fas fa-shopping-cart"></i></button>
   </nav>
