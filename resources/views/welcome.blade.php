17811bb8b480b3dfa9da5f34fe458896350b65f3
Romain Valla <romainvalla@MacBook-Pro-de-Romain.local>
2019-12-12 11:43:41 +0100

    update

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -24,6 +24,174 @@
     </div>
   </div>
   <div id="content-2">
+    <div class="top">
+      <div class="row">
+        <div class="col-md-6">
+          <p class="title">Best Experiences</p>
+        </div>
+        <div class="col-md-6 text-right">
+          <button class="btn-large op" type="button" name="button">View more</button>
+        </div>
+      </div>
+    </div>
+    <div class="main">
+      <div class="row">
+        <div class="col-md-auto">
+          <div class="pst">
+            <div class="pst-img pic-1"></div>
+            <div class="pst-title">
+              <p>Tomatos</p>
+            </div>
+            <div class="pst-desc">
+              <p>Fresh organic tomatoes Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+            </div>
+            <div class="pst-pos">
+              <div class="row">
+                <i class="fas fa-location-arrow"></i><p>Marseille</p>
+              </div>
+            </div>
+            <div class="pst-vendor">
+              <div class="row">
+                <i class="fas fa-user"></i><p>Alexandre</p>
+              </div>
+            </div>
+            <div class="pst-price">
+              <div class="row">
+                <p>€ 7.99 <span>/kg</span></p>
+              </div>
+            </div>
+            <div class="row">
+              <div class="col-md-8" style="padding-right: 0;">
+                <div class="pst-buy">
+                  <button type="button" name="button">Buy now</button>
+                </div>
+              </div>
+              <div class="col-md-4" style="padding-left: 0;">
+                <div class="pst-more">
+                  <button type="button" name="button"><i class="fas fa-arrow-right"></i></button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-auto">
+          <div class="pst">
+            <div class="pst-img pic-2"></div>
+            <div class="pst-title">
+              <p>Carrot</p>
+            </div>
+            <div class="pst-desc">
+              <p>Fresh organic carrot's Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+            </div>
+            <div class="pst-pos">
+              <div class="row">
+                <i class="fas fa-location-arrow"></i><p>Paris</p>
+              </div>
+            </div>
+            <div class="pst-vendor">
+              <div class="row">
+                <i class="fas fa-user"></i><p>Romain</p>
+              </div>
+            </div>
+            <div class="pst-price">
+              <div class="row">
+                <p>€ 7.99 <span>/kg</span></p>
+              </div>
+            </div>
+            <div class="row">
+              <div class="col-md-8" style="padding-right: 0;">
+                <div class="pst-buy">
+                  <button type="button" name="button">Buy now</button>
+                </div>
+              </div>
+              <div class="col-md-4" style="padding-left: 0;">
+                <div class="pst-more">
+                  <button type="button" name="button"><i class="fas fa-arrow-right"></i></button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-auto">
+          <div class="pst">
+            <div class="pst-img pic-2"></div>
+            <div class="pst-title">
+              <p>Carrot</p>
+            </div>
+            <div class="pst-desc">
+              <p>Fresh organic carrot's Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+            </div>
+            <div class="pst-pos">
+              <div class="row">
+                <i class="fas fa-location-arrow"></i><p>Paris</p>
+              </div>
+            </div>
+            <div class="pst-vendor">
+              <div class="row">
+                <i class="fas fa-user"></i><p>Romain</p>
+              </div>
+            </div>
+            <div class="pst-price">
+              <div class="row">
+                <p>€ 7.99 <span>/kg</span></p>
+              </div>
+            </div>
+            <div class="row">
+              <div class="col-md-8" style="padding-right: 0;">
+                <div class="pst-buy">
+                  <button type="button" name="button">Buy now</button>
+                </div>
+              </div>
+              <div class="col-md-4" style="padding-left: 0;">
+                <div class="pst-more">
+                  <button type="button" name="button"><i class="fas fa-arrow-right"></i></button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-auto">
+          <div class="pst">
+            <div class="pst-img pic-2"></div>
+            <div class="pst-title">
+              <p>Carrot</p>
+            </div>
+            <div class="pst-desc">
+              <p>Fresh organic carrot's Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+            </div>
+            <div class="pst-pos">
+              <div class="row">
+                <i class="fas fa-location-arrow"></i><p>Paris</p>
+              </div>
+            </div>
+            <div class="pst-vendor">
+              <div class="row">
+                <i class="fas fa-user"></i><p>Romain</p>
+              </div>
+            </div>
+            <div class="pst-price">
+              <div class="row">
+                <p>€ 7.99 <span>/kg</span></p>
+              </div>
+            </div>
+            <div class="row">
+              <div class="col-md-8" style="padding-right: 0;">
+                <div class="pst-buy">
+                  <button type="button" name="button">Buy now</button>
+                </div>
+              </div>
+              <div class="col-md-4" style="padding-left: 0;">
+                <div class="pst-more">
+                  <button type="button" name="button"><i class="fas fa-arrow-right"></i></button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div id="content-3">
 
   </div>
 @include('layouts.footer')
