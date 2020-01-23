05c2f5ae7c8baef16f5231635301dea51a42ec49
Romain Valla <romainvalla@MacBook-Pro-de-Romain.local>
2020-01-23 09:15:21 +0100

    change

--- resources/views/layouts/navbarDesktop.blade.php
+++ resources/views/layouts/navbarDesktop.blade.php
@@ -14,11 +14,8 @@
       </div>
       <div class="col-md-3 text-center menu">
         <button type="button" name="button" onclick="window.location.href='{{ url('favorite') }}'"><i class="fas fa-heart"></i></button>
-        @if()
         <button type="button" name="button" onclick="window.location.href='{{ url('test/register1234') }}'"><i class="fas fa-user-plus"></i></button>
-        @else
         <button type="button" name="button" onclick="window.location.href='{{ url('test/register1234') }}'"><i class="fas fa-user"></i></button>
-        @endif
         <button type="button" name="button" onclick="window.location.href='{{ url('cart') }}'"><i class="fas fa-shopping-bag"></i></button>
       </div>
     </div>
