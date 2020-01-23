e328c0b88883488a9fd1751cacca71908e30e2bd
Flaker <aceretto@nagra-info.com>
2020-01-23 09:30:02 +0100

    replqce footer bugged

--- resources/views/layouts/navbarMobile.blade.php
+++ resources/views/layouts/navbarMobile.blade.php
@@ -8,16 +8,8 @@
         <h1>Healthy's</h1>
       </div>
       <div class="col-4 text-center menu">
-<<<<<<< HEAD
         <button type="button" name="button" onclick="openFav()"><i class="fas fa-heart"></i></button>
         <button type="button" name="button" onclick="openSignup()"><i class="fas fa-user"></i></button>
-=======
-<<<<<<< HEAD
-        <button type="button" name="button" onclick="openFav()"><i class="fas fa-heart"></i></button>
-        <button type="button" name="button" onclick="openSignup()"><i class="fas fa-user"></i></button>
-=======
->>>>>>> Remy
->>>>>>> 2bcde7156e7921ca0d5c15660b2e0f60b54cda82
         <button type="button" name="button" onclick="openCart()"><i class="fas fa-shopping-bag"></i></button>
       </div>
     </div>
