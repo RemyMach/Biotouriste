6c07d97991d1e8d3b47fbcab46a806d505359a07
romainvalla <romainvallaofficiel@gmail.com>
2020-02-06 09:26:35 +0100

    final design

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -61,7 +61,7 @@
                   <p>Comments : (34)</p>
                 </div>
                 <div class="view">
-                  <button class="btn_top_vendor" type="button" name="button">View Profil</button>
+                  <button class="btn_top_vendor" type="button" name="button" onclick="">View Profil</button>
                 </div>
               </div>
             </div>
@@ -85,7 +85,7 @@
                   <p>Comments : (34)</p>
                 </div>
                 <div class="view">
-                  <button class="btn_top_vendor" type="button" name="button">View Profil</button>
+                  <button class="btn_top_vendor" type="button" name="button" onclick="">View Profil</button>
                 </div>
               </div>
             </div>
@@ -109,7 +109,7 @@
                   <p>Comments : (34)</p>
                 </div>
                 <div class="view">
-                  <button class="btn_top_vendor" type="button" name="button">View Profil</button>
+                  <button class="btn_top_vendor" type="button" name="button" onclick="">View Profil</button>
                 </div>
               </div>
             </div>
@@ -133,7 +133,7 @@
                   <p>Comments : (34)</p>
                 </div>
                 <div class="view">
-                  <button class="btn_top_vendor" type="button" name="button">View Profil</button>
+                  <button class="btn_top_vendor" type="button" name="button" onclick="">View Profil</button>
                 </div>
               </div>
             </div>
@@ -157,7 +157,7 @@
                   <p>Comments : (34)</p>
                 </div>
                 <div class="view">
-                  <button class="btn_top_vendor" type="button" name="button">View Profil</button>
+                  <button class="btn_top_vendor" type="button" name="button" onclick="">View Profil</button>
                 </div>
               </div>
             </div>
@@ -166,7 +166,7 @@
         <div class="cta">
           <div class="row" style="margin:0;">
             <div class="col-xs-12 col-sm-12 col-md-2 offset-md-5 text-center">
-              <button type="button" name="button">View all</button>
+              <button type="button" name="button" onclick="">View all</button>
             </div>
           </div>
         </div>
@@ -176,21 +176,102 @@
 </div>
 <div id="article">
   <div class="row" style="margin:0;">
-    <div class="col-xs-12 col-sm-12 col-md-8 offset-md-2 text-center">
+    <div class="col-xs-12 col-sm-12 col-md-12 text-center">
       <div id="content_4">
         <div data-aos="fade-up">
           <h2>Lastest articles</h2>
           <div class="line"></div>
         </div>
+        <div class="col-xs-12 col-sm-12 col-md-10 offset-md-1">
+          <div class="row" style="margin:0;">
+            <div class="col-md-4">
+              <div class="column">
+                <div class="article" data-aos="fade-up">
+                  <div class="pic" style="background-image:url(../img/article/article_3.jpg)"></div>
+                  <div class="content">
+                    <div class="title">
+                      <h3>News 2020</h3>
+                    </div>
+                    <div class="desc">
+                      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                    </div>
+                    <div class="view">
+                      <button class="btn_article" type="button" name="button" onclick="">Read More</button>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="col-md-4">
+              <div class="column">
+                <div class="article" data-aos="fade-down">
+                  <div class="pic" style="background-image:url(../img/article/article_1.jpg)"></div>
+                  <div class="content">
+                    <div class="title">
+                      <h3>News 2020</h3>
+                    </div>
+                    <div class="desc">
+                      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                    </div>
+                    <div class="view">
+                      <button class="btn_article" type="button" name="button" onclick="">Read More</button>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="col-md-4">
+              <div class="column">
+                <div class="article" data-aos="fade-up">
+                  <div class="pic" style="background-image:url(../img/article/article_2.jpg)"></div>
+                  <div class="content">
+                    <div class="title">
+                      <h3>News 2020</h3>
+                    </div>
+                    <div class="desc">
+                      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                    </div>
+                    <div class="view">
+                      <button class="btn_article" type="button" name="button" onclick="">Read More</button>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="cta">
+          <div class="row" style="margin:0;">
+            <div class="col-xs-12 col-sm-12 col-md-2 offset-md-5 text-center">
+              <button type="button" name="button" onclick="">View all</button>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   </div>
 </div>
-<div id="content_5">
+<div id="contact">
   <div class="row" style="margin:0;">
-    <div class="col-xs-12 col-sm-12 col-md-8 offset-md-2 text-center">
-      <div id="contact">
+    <div class="col-xs-12 col-sm-12 col-md-10 offset-md-1 text-center">
+      <div id="content_5" data-aos="fade-right">
+        <div class="row" style="margin:0;">
+          <div class="col-md-6">
+            
+          </div>
+          <div class="col-md-6">
 
+          </div>
+        </div>
+        <h3>About Us</h3>
+        <div class="line"></div>
+        <h2>What is Healthy's ?</h2>
+        <div class="col-md-6 offset-md-3 text-center">
+          <p>No matter where you are, in travel or home, you will be able to access to the best selection of organic producers. You will be able to buy your products from the plateform, also from our application available <a href="#">here</a>.</p>
+          <p>The first step is to reference your adress to have access to our listed producers in the area. Then you will be able to select your list of product in different type of unit.</p>
+          <p>Eating healthy has never been so simple !</p>
+          <button type="button" name="button" onclick="window.location.href='{{ url('about') }}'">Read more</button>
+        </div>
       </div>
     </div>
   </div>
@@ -204,32 +285,32 @@
 
 
 <script type="text/javascript">
-  function announces() {
-    document.getElementById('announces').style.display = "block";
-    document.getElementById('messages').style.display = "none";
-    document.getElementById('products').style.display = "none";
+function announces() {
+  document.getElementById('announces').style.display = "block";
+  document.getElementById('messages').style.display = "none";
+  document.getElementById('products').style.display = "none";
 
-    document.getElementById('btn_a').parentElement.classList.add("active");
-    document.getElementById('btn_m').parentElement.classList.remove("active");
-    document.getElementById('btn_p').parentElement.classList.remove("active");
-  }
-  function messages() {
-    document.getElementById('announces').style.display = "none";
-    document.getElementById('messages').style.display = "block";
-    document.getElementById('products').style.display = "none";
+  document.getElementById('btn_a').parentElement.classList.add("active");
+  document.getElementById('btn_m').parentElement.classList.remove("active");
+  document.getElementById('btn_p').parentElement.classList.remove("active");
+}
+function messages() {
+  document.getElementById('announces').style.display = "none";
+  document.getElementById('messages').style.display = "block";
+  document.getElementById('products').style.display = "none";
 
-    document.getElementById('btn_a').parentElement.classList.remove("active");
-    document.getElementById('btn_m').parentElement.classList.add("active");
-    document.getElementById('btn_p').parentElement.classList.remove("active");
-  }
-  function products() {
-    document.getElementById('announces').style.display = "none";
-    document.getElementById('messages').style.display = "none";
-    document.getElementById('products').style.display = "block";
+  document.getElementById('btn_a').parentElement.classList.remove("active");
+  document.getElementById('btn_m').parentElement.classList.add("active");
+  document.getElementById('btn_p').parentElement.classList.remove("active");
+}
+function products() {
+  document.getElementById('announces').style.display = "none";
+  document.getElementById('messages').style.display = "none";
+  document.getElementById('products').style.display = "block";
 
-    document.getElementById('btn_a').parentElement.classList.remove("active");
-    document.getElementById('btn_m').parentElement.classList.remove("active");
-    document.getElementById('btn_p').parentElement.classList.add("active");
-  }
+  document.getElementById('btn_a').parentElement.classList.remove("active");
+  document.getElementById('btn_m').parentElement.classList.remove("active");
+  document.getElementById('btn_p').parentElement.classList.add("active");
+}
 </script>
 @include('layouts.footer')
