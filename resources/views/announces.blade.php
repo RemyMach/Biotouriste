8123c9ff82fb3be5d7f78d4e912bab01c132aa29
sami <sami.benyounes.pro@gmail.com>
2020-02-06 13:58:11 +0100

    https for geoname

--- resources/views/announces.blade.php
+++ resources/views/announces.blade.php
@@ -52,7 +52,7 @@ function findByCity(cityData){
 
 function findCityData(){
     $.ajax({
-        url: 'https://api.geonames.org/searchJSON?&',
+        url: 'https://secure.geonames.org/searchJSON?&',
         type: 'GET',
         data: {q : $('#cityZone').val(), maxRows: 1, username: 'biotouriste'},
         dataType: "json",
@@ -65,7 +65,7 @@ function findCityData(){
 
 function filterByCategorieProduct(categorie){
     $.ajax({
-        url: 'https://api.geonames.org/searchJSON?&',
+        url: 'https://secure.geonames.org/searchJSON?&',
         type: 'GET',
         data: {q : $('#cityZone').val(), maxRows: 1, username: 'biotouriste'},
         dataType: "json",
