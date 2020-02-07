80e2ec28f94b2feec19cca7e2706af583c9ea39e
sami <sami.benyounes.pro@gmail.com>
2020-02-07 09:28:21 +0100

    products in caps

--- app/Repositories/AnnounceRepository.php
+++ app/Repositories/AnnounceRepository.php
@@ -37,7 +37,7 @@ class AnnounceRepository extends BaseRepository
         } else {
             $qb = DB::table('Announces')
                 ->select('Announces.*')
-                ->join('products', 'announces.products_idproduct', '=', 'products.idproduct');
+                ->join('Products', 'announces.products_idproduct', '=', 'products.idproduct');
                 if($idCategorie == 0){
                     $qb->where('product_categories_idproduct_category', [1,2,3,4,5,6]);
                 }else{
