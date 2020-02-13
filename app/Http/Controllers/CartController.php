6688cad191cbd31120b4ec764bf8a4823e82522c
Flaker <aceretto@nagra-info.com>
2020-02-13 16:20:39 +0100

    before merge develop into stripe

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -36,9 +36,9 @@ class CartController extends Controller
 //        //Session::forget('cart');
 //        $prout = Announce::find([1]);
 //        dd($prout);
-        $prout = ['idAnnounce' => 1, "announce_name" => "Test1", 'announce_comment' => "Testcomment" , 'announce_price' => "25.88"];
+        $prout = ['idAnnounce' => 1, "announce_name" => "Test1", 'announce_quantity' => 3 , 'announce_price' => "25.88"];
         $request->session()->push('cart', $prout);
-        $prout = ['idAnnounce' => 3, "announce_name" => "Test3", 'announce_comment' => "Testcommentblabla" , 'announce_price' => "2.88"];
+        $prout = ['idAnnounce' => 3, "announce_name" => "Test3", 'announce_quantity' => 2 , 'announce_price' => "2.88"];
         $request->session()->push('cart', $prout);
         $announces = $request->session()->get('cart');
 
@@ -62,10 +62,35 @@ class CartController extends Controller
 
     public function countCart(Request $request){
         $cart = session()->get('cart');
-        $cart = count($cart);
+        $nbannouces = count($cart);
         if($cart === 0){
             return redirect('cart');
         }
-        dd($cart);
+
+        session()->put('number', $nbannouces);
+        return redirect('stripe');
+    }
+
+    public function qantmore(Request $request){
+        $index = $request->get('index');
+        $announces = session()->get('cart');
+        $announces[$index]['announce_quantity'] = $announces[$index]['announce_quantity'] + 1;
+        session()->put('cart', $announces);
+        return redirect()->route('cart');
+    }
+
+    public function qantless(Request $request)
+    {
+        $index = $request->get('index');
+        $announces = session()->get('cart');
+        $announces[$index]['announce_quantity'] = $announces[$index]['announce_quantity'] - 1;
+        if ($announces[$index]['announce_quantity'] === 0) {
+            return redirect('cart');
+        }elseif (DB::table('Announces')->select('announce_quantity')->where("idAnnounce == $announces[$index]['idAnnounce']")->get() ){
+
+        } else {
+            session()->put('cart', $announces);
+            return redirect()->route('cart');
+        }
     }
 }
--- resources/views/cart.blade.php
+++ resources/views/cart.blade.php
@@ -1,7 +1,6 @@
 @include('layouts.header')
 @include('layouts.navbarDesktop')
 @include('layouts.navbarMobile')
-@if(session()->has('user'))
 
 <div id="cart">
   <div class="col-md-12 text-center">
@@ -9,6 +8,8 @@
       <div class="col-xs-12 col-sm-12 col-md-10 offset-md-1 text-center">
         <div class="card">
           <div id="product">
+            @if($announces != null)
+
             <h3>Cart</h3>
               <table class="table">
                 <thead>
@@ -20,37 +21,45 @@
                     <th scope="col"></th>
                   </tr>
                 </thead>
+                @foreach($announces as $key => $announce)
+
                 <tbody>
                   <tr>
-                    <td><img src="../img/product/blueberry.png" alt=""> Blueberry</td>
-                    <td><input type="number" name="" value="4"></td>
-                    <td>$15</td>
-                    <td>$60</td>
-                    <td><button type="button" name="button">Remove</button></td>
+                    <td><img src="../img/product/blueberry.png" alt="">{{ $announce['announce_name'] }}</td>
+                    <td>
+                      {{ $announce['announce_quantity'] }}
+                      <form action="qantmore" method="get">
+                        <input type="hidden" value="{{ $key }}" name="index">
+                      <button class="+button" value="" onclick="window.location.href='{!!route('qantmore')!!}'">+</button>
+                      </form>
+                      <form action="qantless" method="get">
+                        <input type="hidden" value="{{ $key }}" name="index">
+                      <button class="-button" onclick="window.location.href='{!!route('qantless')!!}'">-</button>
+                      </form>
+                    </td>
+                    <td>{{ $announce['announce_price'] }}$</td>
+                    <td>{{ $total = $announce['announce_quantity'] * $announce['announce_price'] }}$</td>
+                    <form class="" action="cart/remove" method="get">
+                    <input type="hidden" value="{{ $key }}" name="index">
+                    <td><button type="submit"  name="button">Remove</button></td>
+                    </form>
                   </tr>
-                  <tr>
-                    <td><img src="../img/product/blueberry.png" alt=""> Blueberry</td>
-                    <td><input type="number" name="" value="4"></td>
-                    <td>$15</td>
-                    <td>$60</td>
-                    <td><button type="button" name="button">Remove</button></td>
-                  </tr>
-                  <tr>
-                    <td><img src="../img/product/blueberry.png" alt=""> Blueberry</td>
-                    <td><input type="number" name="" value="4"></td>
-                    <td>$15</td>
-                    <td>$60</td>
-                    <td><button type="button" name="button">Remove</button></td>
-                  </tr>
+
                 </tbody>
+                @endforeach
               </table>
           </div>
         </div>
+
+
         <div class="cta">
           <div class="row">
             <div class="col-md-12">
               <button type="button" name="button" onclick="window.location.href='{{ url('announces') }}'">Continue shopping</button>
-              <button type="submit" name="button" formmethod="post">Proceed to payment</button>
+              <button type="submit" name="button" onclick="window.location.href='{!!route('ccart')!!}'" formmethod="post">Proceed to payment</button>
+              @else
+                <h2>Your cart is empty</h2>
+              @endif
             </div>
           </div>
         </div>
