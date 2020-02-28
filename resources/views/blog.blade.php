83416968bc5e14bef57bab28b7e9e6bf7593aa13
Romain valla <romainvalla@MacBook-Pro-de-Romain.local>
2020-02-28 10:51:08 +0100

    blog front

--- resources/views/blog.blade.php
+++ resources/views/blog.blade.php
@@ -6,7 +6,13 @@
     <div class="row" style="margin:0;">
       <div class="col-xs-12 col-sm-12 col-md-10 offset-md-1 text-center">
         <div class="row" style="margin:0;">
+          <div class="card">
+            <div id="article">
+              <div class="">
 
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
