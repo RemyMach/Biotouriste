c7b9f11a276ddfcd4ca8e6c8136de7f44740d4a3
sami <sami.benyounes.pro@gmail.com>
2020-02-28 11:58:03 +0100

    navbar Message

--- resources/views/layouts/navbarDesktop.blade.php
+++ resources/views/layouts/navbarDesktop.blade.php
@@ -10,7 +10,6 @@
         @if(session('active_status'))
           @if(session('active_status')->status_user_label == 'Seller')
           <a href="{{ url('/announce/historySeller') }}">My announces</a>
-          <a href="{{ url('/message') }}">Messages</a>
           @endif
         @endif
         <a href="{{ url('/#about') }}">About us</a>
@@ -20,6 +19,7 @@
       <div class="col-md-3 text-center menu">
         @if(Session::has('user'))
           <button type="button" name="button" onclick="window.location.href='{{ url('favorite') }}'"><i class="fas fa-heart"></i></button>
+          <button type="submit" name="button" onclick="window.location.href='{{ url('/message') }}'"><i class="fas fa-comments"></i></button>
           <button type="button" name="button" onclick="window.location.href='{{ url('profil') }}'"><i class="fas fa-user"></i></button>
           <button type="button" name="button" onclick="window.location.href='{{ url('cart') }}'"><i class="fas fa-shopping-bag"></i></button>
           <button type="submit" name="button" onclick="window.location.href='{{ url('logout') }}'"><i class="fas fa-sign-out-alt"></i></button>
